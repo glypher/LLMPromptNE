a040ea9f7b148ad293613fce547912b4de00207f
EC2 Default User <ec2-user@ip-172-31-41-71.ec2.internal>
2024-06-30 00:43:34 +0000

    Remove unwanted logs

--- app/model.py
+++ app/model.py
@@ -5,10 +5,10 @@ import logging
 import sys
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
+handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', "%Y/%m/%d %H:%M:%S")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
